8ed5404e6894b650397f0bb7571a524e991a820a
Lenny Peters <mr.lennypeters@gmail.com>
2014-06-14 12:55:08 -0700

    Changes to home and index

--- index.php
+++ index.php
@@ -1,40 +1,68 @@
 <?php get_header(); ?>
 
-<!-- /END HEADER -->   
+<!-- /END HEADER -->  
 
-<main id="blogroll">
-  <!--START THE LOOP-->
-  <?php if (have_posts() ) : while (have_posts() ) : the_post(); ?>
-  <div class="row">
+<?php if (have_posts()) : ?>
+  <?php while (have_posts()) : the_post(); ?>
 
-    <article class="large-6 columns">
-      <h3 class="aligncenter"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-      <?php the_content(); ?>
-      <div class="metadata">
-        <span class="author">By <?php the_author_posts_link(); ?></span><br/>
-        <span class="date"><?php the_time('F jS, Y'); ?></span><br/>
-        <span class="catagories">Catagories <?php the_catagory(' &raquo; '); ?></span><br/>
-        <?php
-        $taglist = get_the_tags();
-        if ($taglist) {
-          echo "<span class=\"tags\">Tags ";
-          $lasttag = end($taglist);
-          foreach ($taglist as $val) {
-            echo "<a href=\"".get_tag_link($val->term_id)."\" rel=\"tag\">".$val->name."</a>";
-            if($val !== $lasttag){
-              echo ", ";
-            }
-            echo "</span>";
-          }
-        }
-        ?>
+
+    <main id="blogroll">
+      <div class="row">
+        <div class="small-12 columns">
+          <h2>Blog  //</h2>
+        </div>
+        <div class="small-12 columns">
+          <?php get_search_form(); ?>
+        </div>
       </div>
-    </article>
 
-  </div>  
-</main>
+      <div class="row">
+        <article class="large-6 columns">
+          <a href="<?php the_permalink(); ?>"><h3 class="aligncenter"><?php the_title(); ?></h3></a>
+          <em><?php the_time('jS F Y'); ?> | <? the_category(', ');?> |</em>
+          <figure>
+            <a href=""><img src="http://placekitten.com/480/270" alt=""/></a>
+          </figure>
+          <p class="lead"><?php the_content(''); ?></p>
+          <a href="<?php the_permalink(); ?>">Continue Reading &raquo;</a>
+        </article>
 
-<!--/END LOOP-->
+      <?php endwhile; ?>
+
+      <nav id="pagination">
+        <ul>
+          <li class="older"><?php next_posts_link('Older Posts'); ?></li>
+          <li class="newer"><?php next_posts_link('Newer Posts'); ?></li>
+        </ul>
+      </nav><!-- END NAVIGATION FOR PAGINATION -->
+
+    <?php else : ?>
+
+      <article class="nothing">
+        <h2>Nothing Found</h2>
+        <p>Sorry, but you are looking for something that isn't here.</p>
+        <p><a href="<?php echo get_option('home'); ?>">Return to the homepage</a></p>
+      </article>
+
+    <?php endif; ?>
+
+    </div><!-- row -->
+
+
+
+      
+
+
+
+
+  <div class="row">
+    <div class="small-3 small-centered columns">
+      <div id="btt">
+        <a href="#Top"><button>Back to Top</button></a>
+      </div>
+    </div>
+  </div>		
+
 
 <!-- BEGIN FOOTER -->
 
